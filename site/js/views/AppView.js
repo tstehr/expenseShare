c5575cd7c25b80c9b34e6db5244b1604f7eb0e81
Tilman Stehr <tilman.stehr@gmail.com>
2013-04-25 11:19:26 +0000

    Adding persentation file

--- site/js/views/AppView.js
+++ site/js/views/AppView.js
@@ -1,5 +1,7 @@
 var app = app || {};
 
 app.AppView = Backbone.View.extend({
-	// TODO implement root view to hold main UI, should provide methods to be called by Router
+	initialize: function () {
+		
+	}
 });
