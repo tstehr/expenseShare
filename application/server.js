6de9ac0cc91a0c2f70cfe89792d000565460beea
Tilman Stehr <tilman.stehr@gmail.com>
2013-04-30 11:55:07 +0000

    Server now supports getting and updating of persons (without db layer)

--- application/server.js
+++ application/server.js
@@ -15,22 +15,58 @@ app.configure(function() {
 	app.use(app.router);
 
 	//Where to serve static content
-	app.use(express.static( path.join( application_root, 'site')));
+	app.use(express.static(path.join(application_root, 'site')));
 
 	//Show all errors in development
-	app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+	app.use(express.errorHandler({ 
+		dumpExceptions: true, 
+		showStack: true 
+	}));
 });
 
-app.get('/api/persons', function (req, res) {
-	res.set('Content-type', 'application/json');
 
-	res.send(JSON.stringify([
+/**
+ * since we don't really care about data persistance right now, just store all data into an object server side
+*/
+(function () {
+	var getId = (function () {
+		var id = 1;
+		return function () {
+			return id++;
+		};
+	}());
+
+	var persons = [
 		{
-			id: 1,
+			id: getId(),
 			name: "abc"
+		},
+		{
+			id: getId(),
+			name: "def"
 		}
-	]));
-});
+	];
+
+	// getting all persons
+	app.get('/api/persons', function (req, res) {
+		res.set('Content-type', 'application/json');
+
+		res.send(JSON.stringify(persons));
+	});
+
+	// updating an existing person
+	app.put('/api/persons/:id', function (req, res) {
+		persons.forEach(function (p) {
+			if (p.id === parseInt(req.params.id, 10)) {
+				Object.keys(req.body).forEach(function (key) {
+					p[key] = req.body[key];
+				});
+			}
+		});
+	});
+}());
+
+
 
 //Start server
 var port = 4242;
