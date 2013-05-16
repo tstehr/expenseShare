c017c1eecf3765c33b43305dfe315e7772f81765
Tilman Stehr <tilman.stehr@gmail.com>
2013-05-16 21:35:33 +0000

    Addedd comment describing routes

--- application/site/js/routers/AppRouter.js
+++ application/site/js/routers/AppRouter.js
@@ -3,5 +3,8 @@ var app = app || {};
 // TODO implement this. should interact with AppView
 // Idea: http://addyosmani.github.io/backbone-fundamentals/#routers
 app.ExpenseShareRouter = Backbone.Router.extend({
-
+// Routen
+// /month/[YYYY]-[dd]  app.appView.showMonth
+// /expense/[id]
+// /persons/
 });
