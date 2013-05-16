9e1bda50ac6413252c38fb47e92345c8aebf1358
Christian Spreng <c.spreng@zoho.com>
2013-05-16 23:16:22 +0000

    Hinzufügen eines Router mit einer Route für die Ansicht der Monate

--- application/site/js/routers/AppRouter.js
+++ application/site/js/routers/AppRouter.js
@@ -2,9 +2,20 @@ var app = app || {};
 
 // TODO implement this. should interact with AppView
 // Idea: http://addyosmani.github.io/backbone-fundamentals/#routers
-app.ExpenseShareRouter = Backbone.Router.extend({
+//app.ExpenseShareRouter = Backbone.Router.extend({
+//	"month" : 
 // Routen
 // /month/[YYYY]-[dd]  app.appView.showMonth
 // /expense/[id]
 // /persons/
-});
+//});
+
+var expenseRouter = Backbone.Router.extend({
+	routes: {
+		"monthView/:date" : "showByDate"
+	},
+	showByDate: function(date){
+	//	document.write(date);
+		app.appView.showMonth(date);
+	},
+	});
