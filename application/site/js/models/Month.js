19d8da46d8418608293f527f8cdf739cfc8df367
Tilman Stehr <tilman.stehr@gmail.com>
2013-05-17 18:15:28 +0000

    Adding algorithm for distributing pam

--- application/site/js/models/Month.js
+++ application/site/js/models/Month.js
@@ -66,10 +66,71 @@ app.Month = Backbone.RelationalModel.extend({
 		};
 	},
 	distributePam: function(pam) {
-		var p = _.groupBy(pam, function (v) {
-			return 1;
+		var MAX_CHANGE_FRACTION = 1, MAX_ITERATIONS = 1e4, CUTOFF = 1e-4;
+
+		var personCids, changes, transfers, maxPerPerson, randomness, change, sd, p1, p2, pFrom, pTo, i;
+
+		personCids = Object.keys(pam);
+
+		changes = {};
+		transfers = {};
+		personCids.forEach(function (personCid) {
+			changes[personCid] = 0;
+			transfers[personCid] = {};
 		});
-		console.log(p);
+
+		maxPerPerson = _.max(pam, function (x) {
+			return Math.abs(x);
+		});
+
+		randomness = 1;
+
+		i = 0;
+
+		do {
+			change = (maxPerPerson * MAX_CHANGE_FRACTION) * (Math.random() * randomness);
+
+			p1 = personCids[app.Util.randomInt(0, personCids.length - 1)];
+			do {
+				p2 = personCids[app.Util.randomInt(0, personCids.length - 1)];
+			} while (p2 === p1);
+
+			if (pam[p1] + changes[p1] < pam[p2] + changes[p2]) {
+				pFrom = p1;
+				pTo = p2;
+			} else {
+				pFrom = p2;
+				pTo = p1;
+			}
+			changes[pFrom] = (changes[pFrom] || 0) + change;
+			changes[pTo] = (changes[pTo] || 0) - change;
+			transfers[pFrom][pTo] = (transfers[pFrom][pTo] || 0) + change;
+
+			sd = Math.sqrt(_.reduce(personCids, function (memo, personCid) {
+				return memo + Math.pow(pam[personCid] + (changes[personCid] || 0), 2);
+			}, 0) / personCids.length) / maxPerPerson;
+			
+			randomness = Math.exp(sd) - 1;
+
+			i++;
+		} while (sd > CUTOFF && i < MAX_ITERATIONS);
+
+		console.group(i, sd, randomness);
+		console.log(pam);
+		console.log(changes);
+		console.log(transfers);
+		console.groupEnd();
+	},
+	randomPam: function(cids) {
+		var MAX = 2000, IT = 500;
+		var pam = {}, p1, p2;
+
+		for (var i; i < IT; i++) {
+			p1 = personCids[app.Util.randomInt(0, personCids.length - 1)];
+			do {
+				p2 = personCids[app.Util.randomInt(0, personCids.length - 1)];
+			} while (p2 === p1);
+		}
 	},
 	toJSONDecorated: function() {
 		var that = this;
