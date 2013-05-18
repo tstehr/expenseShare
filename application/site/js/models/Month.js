40bad3e1f8e004ba2bd41cd119e59900f5535edd
Tilman Stehr <tilman.stehr@gmail.com>
2013-05-18 11:37:11 +0000

    Added working algorithm for distributing pam

--- application/site/js/models/Month.js
+++ application/site/js/models/Month.js
@@ -65,10 +65,12 @@ app.Month = Backbone.RelationalModel.extend({
 			pam: pam
 		};
 	},
-	distributePam: function(pam) {
-		var MAX_CHANGE_FRACTION = 1, MAX_ITERATIONS = 1e4, CUTOFF = 1e-4;
+	distributePamMonteCarlo: function(pam) {
+		console.time("test");
 
-		var personCids, changes, transfers, maxPerPerson, randomness, change, sd, p1, p2, pFrom, pTo, i;
+		var MAX_CHANGE_FRACTION = .5, MAX_ITERATIONS = 1e4, CUTOFF = 1e-4;
+
+		var personCids, changes, transfers, avgPerPerson, randomness, change, deviation,p1, p2, pFrom, pTo, i;
 
 		personCids = Object.keys(pam);
 
@@ -79,21 +81,18 @@ app.Month = Backbone.RelationalModel.extend({
 			transfers[personCid] = {};
 		});
 
-		maxPerPerson = Math.abs(_.max(pam, function (x) {
-			return Math.abs(x);
-		}));
+		avgPerPerson = _.reduce(pam, function (memo, x) {
+			return memo + Math.abs(x)
+		}, 0) / personCids.length;
 
-		randomness = 1;
-
+		roundness = 0;
 		i = 0;
 
 		do {
-			change = (maxPerPerson * MAX_CHANGE_FRACTION) * (Math.random() * randomness);
-
 			p1 = personCids[app.Util.randomInt(0, personCids.length - 1)];
 			do {
 				p2 = personCids[app.Util.randomInt(0, personCids.length - 1)];
-			} while (p2 === p1);
+			} while (app.Util.signum(pam[p1]) === app.Util.signum(pam[p2]));
 
 			if (pam[p1] + changes[p1] < pam[p2] + changes[p2]) {
 				pFrom = p1;
@@ -102,28 +101,103 @@ app.Month = Backbone.RelationalModel.extend({
 				pFrom = p2;
 				pTo = p1;
 			}
+
+			change = (avgPerPerson * MAX_CHANGE_FRACTION) * (Math.random() * randomness);
+
 			changes[pFrom] = (changes[pFrom] || 0) + change;
 			changes[pTo] = (changes[pTo] || 0) - change;
-			transfers[pFrom][pTo] = (transfers[pFrom][pTo] || 0) + change;
 
-			sd = Math.sqrt(_.reduce(personCids, function (memo, personCid) {
-				return memo + Math.pow(pam[personCid] + (changes[personCid] || 0), 2);
-			}, 0) / personCids.length) / maxPerPerson;
-			
-			if (sd < 0) {
-				console.log('WTF!!!!');
+			if (personCids.indexOf(pFrom) > personCids.indexOf(pTo)) {
+				transfers[pFrom][pTo] = (transfers[pFrom][pTo] || 0) + change;
+			} else {
+				transfers[pTo][pFrom] = (transfers[pTo][pFrom] || 0) - change;	
 			}
 
-			randomness = Math.exp(sd) - 1;
+			deviation = Math.sqrt(_.reduce(personCids, function (memo, personCid) {
+				return memo + Math.pow(pam[personCid] + (changes[personCid] || 0), 2);
+			}, 0) / personCids.length) / avgPerPerson;
+
+			randomness = Math.exp(deviation) - 1;
 
 			i++;
-		} while (sd > CUTOFF && i < MAX_ITERATIONS);
+		} while (deviation > CUTOFF && i < MAX_ITERATIONS);
 
-		console.group(i, sd, randomness);
+		
+		personCids.forEach(function (p1) {
+			personCids.forEach(function (p2) {
+				if (transfers[p1][p2] < 0) {
+					transfers[p2][p1] = -transfers[p1][p2];
+					delete transfers[p1][p2];
+				}
+			});
+		});
+
+		console.group(i, deviation, randomness);
+		console.timeEnd("test");
 		console.log(pam);
-		console.log(changes);
+		console.log(changes, _.reduce(changes, function (memo, x) {
+			return memo + x;
+		}, 0));
 		console.log(transfers);
 		console.groupEnd();
+
+		return transfers;
+	},
+	distributePam: function (pam) {
+		console.time('Distribute');
+
+		var INSIGNIFICANCE_CUTOFF = 4;
+
+		var personCids, transfers, high, low;
+
+		personCids = Object.keys(pam);
+		transfers = {};
+		personCids.forEach(function (personCid) {
+			transfers[personCid] = {};
+		});
+
+		high = [];
+		low = [];
+
+		personCids.forEach(function (personCid) {
+			var data = {
+				id: personCid,
+				val: pam[personCid]
+			};
+			if (pam[personCid] < 0) {
+				low.push(data);
+			} else if (pam[personCid] > 0) {
+				high.push(data);
+			}
+		});
+
+		high = _.sortBy(high, 'val').reverse();
+		low = _.sortBy(low, 'val');
+		
+		high.forEach(function (highEl) {
+			low.every(function (lowEl) {
+				var change;
+
+				if (Math.abs(lowEl.val) > INSIGNIFICANCE_CUTOFF) {
+					change = Math.min(highEl.val, Math.abs(lowEl.val));
+
+					highEl.val -= change;
+					lowEl.val += change;
+
+					transfers[lowEl.id][highEl.id] = change;
+
+					if (highEl.val <= INSIGNIFICANCE_CUTOFF) {
+						return false;
+					}
+				}
+				return true;
+			});
+		});
+
+		console.timeEnd('Distribute');
+		console.log(high, low);
+
+		return transfers;
 	},
 	toJSONDecorated: function() {
 		var that = this;
