4ca8e222d838d76c1993c7ce4a4490f64a531133
Christian Spreng <c.spreng@zoho.com>
2013-06-09 13:34:26 +0000

    Anpassen des server für eine verbindung zu einem MySQL Server Benutzername und Password sind der server.js zu entnehmen

--- application/server.js
+++ application/server.js
@@ -1,7 +1,8 @@
 // Module dependencies.
 var application_root = __dirname,
 	express = require('express'), //Web framework
-	path = require('path'); //Utilities for dealing with file paths
+	path = require('path'), //Utilities for dealing with file paths
+	mysql = require('mysql'); //Database access
 
 //Create server
 var app = express();
@@ -23,12 +24,62 @@ app.use(express.errorHandler({
 	showStack: true 
 }));
 
-
-// insert db-code here...
-
+//Connect to mySQL Server
+var connect = function(app){
+	
+	
+//	console.log(mysql);
+	var connection = mysql.createConnection({
+		host : 'localhost',
+		user : 'expense',
+		password : 'share'
+	});
+	return connection;
+//	console.log('Connecting to Server');
+//	connection.connect();
+//	connection.query('insert into test.hallo values("Ulli",2)',function(err,rows,fields){
+//		if(err) throw err;
+//		console.log(rows);
+//	});
+//	connection.query('select * from test.hallo as result', function(err,rows,fields){
+//		if(err) throw err;
+//		console.log(rows);
+//	});
+//	connection.end();
+//	console.log('Connection terminatet');
+};
 //Start server
 var port = 4242;
 
 app.listen(port, function() {
+	app.connection = connect(this);
+	connection.connect();
+//	var app.connection = connection;
+//	connection.end();
 	console.log('Express server listening on port %d in %s mode', port, app.settings.env );
+	
 });
+
+
+
+var createRestInterface = function(app, name, querry){
+	console.log("Createing RI with querry " + querry + " for " + name);
+		var data = app.connection.query(querry);
+	app.get('/api/' + name, function(res, req){
+		
+		res.set('Content-type', 'application/json; charset=utf8');
+		res.send(JSON.stringify(data));
+	});
+		console.log(data);
+};
+
+
+
+createRestInterface(app,"persons","select * from expense_share.persons");
+createRestInterface(app,"expenses","select * from expense_share.expenses");
+
+
+
+
+
+
