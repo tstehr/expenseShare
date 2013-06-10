eb3a513eda221f9eafb108ba3084cec0b78a5c7d
Christian Spreng <c.spreng@zoho.com>
2013-06-10 14:57:46 +0000

    fix route

--- application/server.js
+++ application/server.js
@@ -36,38 +36,30 @@ var connect = function(app){
 	});
 	console.log('Connecting to SQL - Server');
 	connection.connect();
-//	connection.query('select * from test.hallo as result', function(err,rows,fields){
-//		if(err) throw err;
-//		console.log(rows);
-//	});
 	return connection;
-//	connection.query('insert into test.hallo values("Ulli",2)',function(err,rows,fields){
-//		if(err) throw err;
-//		console.log(rows);
-//	});
-//	connection.end();
-//	console.log('Connection terminatet');
 };
 
 var connection = connect(this);
 
-app.get('/month/:id', function (res, req) {
+app.get('/api/months/:id', function (req, res) {
 	console.log('Data asked');
-	var monId = res.params.id;
+	var monId = req.params.id;
 	connection.query('select * from expense_share.expenses where mon = ?',monId,function(err,results){
 		if(err) throw err;
 		var data = {
 			id: monId,
 			expenses: results
 		};
-//		res.set('Content-type', 'application/json; charset=utf8');
-		req.set('Content-type', 'application/json; charset=utf8');
-		req.send(JSON.stringify(data));
-//		res.app.set('Content-type', 'application/json; charset=utf8');
-//		res.app.send(JSON.stringify(data));
+		res.set('Content-type', 'application/json; charset=utf8');
+		res.send(JSON.stringify(data));
 	});
 });
 
+
+
+app.get('*', function (req, res) {
+	res.sendfile(path.join(application_root, 'site/index.html'));
+});
 //Start server
 var port = 4242;
 
