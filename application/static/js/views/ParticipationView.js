116f68d55d5dbe4ff2f4da80a647658659d3c14d
Tilman Stehr <tilman.stehr@gmail.com>
2013-10-05 13:21:31 +0000

    Added debounce for participating state change. Also removed some debug code

--- application/static/js/views/ParticipationView.js
+++ application/static/js/views/ParticipationView.js
@@ -11,24 +11,19 @@ app.ParticipationView = app.AView.extend({
 	},
 
 	initialize: function () {
-		this.listenTo(this.model, 'change pseudochange', function (e) {
-			console.log('Got change event', e, this.model.get('person').get('name'));
-			this.render();
-		}.bind(this));
-		// this.listenTo(this.model, 'destroy', this.dispose);
+		this.listenTo(this.model, 'change pseudochange', this.render);
+		this.listenTo(this.model, 'destroy', this.dispose);
 	},
 	render: function () {
-		console.log('Rerendering', this.model.get('person').get('name'));
-		
 		var data = this.model.toJSONDecorated();
 		data.cid = this.model.cid;
 		this.$el.html(this.template(data));
 		return this;
 	},
-	setParticipating: function (e) {
+	setParticipating: _.debounce(function (e) {
 		this.model.set('participating', e.target.checked);
 		this.model.saveIfNotNew();
-	},
+	}, 50, true),
 	setAmount: function (e) {
 		var am = app.Util.parseCurrency(e.target.value) || 0;
 		if (this.model.get('amount') === am) {
