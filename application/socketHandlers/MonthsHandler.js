3fb6d1b14b5b252e7ca759b24396f1d41e695e79
TilmanS <tilman.stehr@gmail.com>
2014-06-26 10:26:58 +0200

    Null out month api

--- application/socketHandlers/MonthsHandler.js
+++ application/socketHandlers/MonthsHandler.js
@@ -9,41 +9,10 @@ var MonthsHandler = function (socket, pool) {
 };
 
 MonthsHandler.prototype.readMonth = function (socketData, callback) {
-	Q.nmcall(this.pool, 'getConnection')
-		.then(function (connection) {
-			// TODO month id valid?
-			return Q.nmcall(connection, 'query', 'select * from expenses where month = ?', [socketData.id])
-				.then(function (dbData) {
-					var expenses = dbData[0];
-					var promises = [Q.resolve(expenses)];
-					expenses.forEach(function (expense) {
-						promises.push(Q.nmcall(
-							connection, 'query', 
-							'select * from participations where expense = ?', [expense.id]
-						));
-					});
-					return promises;
-				})
-				.spread(function (expenses) {
-					var partsDbData = Array.prototype.slice.call(arguments, 1);
-					partsDbData.forEach(function (dbData, i) {
-						expenses[i].participations = dbData[0];
-					});
-					callback(null, {
-						id: socketData.id,
-						expenses: expenses
-					});
-				})
-				.fin(function () {
-					connection.release();
-				})
-			;
-		})
-		.fail(function () {
-			console.log('Error in readMonth',  arguments);
-			callback(null);
-		})
-	;
+	callback(null, {
+		id: socketData.id,
+		expenses: []
+	});
 };
 
 
