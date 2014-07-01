d74758699b7bcbeef7243789dc5ac911b4d4cdba
TilmanS <tilman.stehr@gmail.com>
2014-07-01 11:41:13 +0200

    Fix indentation

--- initdb.sql
+++ initdb.sql
@@ -5,21 +5,21 @@ use expense_share;
 set names utf8;
 
 create table persons(
-    id int not null auto_increment primary key, 
-    name varchar(50) not null, 
-    hidden boolean not null default false
+	id int not null auto_increment primary key, 
+	name varchar(50) not null, 
+	hidden boolean not null default false
 );
 
 create table expenses(
-    id int not null auto_increment primary key, 
-    description varchar(50) not null, 
-    created timestamp not null
+	id int not null auto_increment primary key, 
+	description varchar(50) not null, 
+	created timestamp not null
 );
 
 create table participations(
-    id int not null auto_increment primary key, 
-    person int not null, 
-    expense int not null, 
-    amount int not null, 
-    participating boolean not null
+	id int not null auto_increment primary key, 
+	person int not null, 
+	expense int not null, 
+	amount int not null, 
+	participating boolean not null
 );
